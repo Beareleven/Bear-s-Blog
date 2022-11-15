8b3951cc4a8c20ebfeadb2694bee74fc88fd3cb3
bear <beareleven@aliyun.com>
2022-11-15 17:42:06 +0800

    ast

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,9 @@ git init
 git add -A
 git commit -m "deploy"
 
-git push -f git@github.com:Beareleven/Bear-s-Blog.git master:gh-pages
+
+# git push -f https://github.com/Beareleven/Bear-s-Blog.git master:gh-pages
+# git push -f git@github.com:Beareleven/Bear-s-Blog.git master:gh-pages
 # git push -f git@gitee.com:saw-string/Bear-s-Blog.git master:gh-pages
 # git push -f git@106.15.188.56:/home/www/website/ts.git master
 git push -f git@39.108.58.119:/home/www/website/blog.git master
